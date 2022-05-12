e2220a021698c2ff93de1883d9d536165d15229d
Tobias Gimpel <tobias.gimpel@justrelate.com>
2022-05-12 13:30:13 +0200

    Export ProfilePage and UserDataProvider

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,15 @@
 import { TicketEntry } from "./Components/TicketEntry";
+import { UserDataProvider } from "./Components/UserDataContext";
 import "./Objs/ChatPage/ChatPageComponent";
 import "./Objs/ChatPage/ChatPageEditingConfig";
 import "./Objs/ChatPage/ChatPageObjClass";
+import "./Objs/ProfilePage/ProfilePageComponent";
+import "./Objs/ProfilePage/ProfilePageEditingConfig";
+import "./Objs/ProfilePage/ProfilePageObjClass";
 import "./Widgets/EditProfileWidget/EditProfileWidgetClass";
 import "./Widgets/EditProfileWidget/EditProfileWidgetComponent";
 import "./Widgets/EditProfileWidget/EditProfileWidgetEditingConfig";
 import "./Widgets/TicketsWidget/TicketsWidgetClass";
 import "./Widgets/TicketsWidget/TicketsWidgetComponent";
 import "./Widgets/TicketsWidget/TicketsWidgetEditingConfig";
-export { TicketEntry };
+export { TicketEntry, UserDataProvider };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { TicketEntry } from "./Components/TicketEntry";
+import { UserDataProvider } from "./Components/UserDataContext";
 import "./Objs/ChatPage/ChatPageComponent";
 import "./Objs/ChatPage/ChatPageEditingConfig";
 import "./Objs/ChatPage/ChatPageObjClass";
+import "./Objs/ProfilePage/ProfilePageComponent";
+import "./Objs/ProfilePage/ProfilePageEditingConfig";
+import "./Objs/ProfilePage/ProfilePageObjClass";
 import "./Widgets/EditProfileWidget/EditProfileWidgetClass";
 import "./Widgets/EditProfileWidget/EditProfileWidgetComponent";
 import "./Widgets/EditProfileWidget/EditProfileWidgetEditingConfig";
@@ -9,4 +13,4 @@ import "./Widgets/TicketsWidget/TicketsWidgetClass";
 import "./Widgets/TicketsWidget/TicketsWidgetComponent";
 import "./Widgets/TicketsWidget/TicketsWidgetEditingConfig";
 
-export { TicketEntry };
+export { TicketEntry, UserDataProvider };
