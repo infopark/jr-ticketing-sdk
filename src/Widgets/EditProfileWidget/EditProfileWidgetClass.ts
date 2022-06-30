b1e4c64c54d859d6ff73e87ee6e5cf1733cf1412
Tobias Gimpel <tobias.gimpel@justrelate.com>
2022-06-30 12:07:00 +0200

    Removed EditProfileWidget because it depends on TeaserBoxWidget

--- src/Widgets/EditProfileWidget/EditProfileWidgetClass.ts
+++
@@ -1,9 +0,0 @@
-import * as Scrivito from "scrivito";
-
-const EditProfileWidget = Scrivito.provideWidgetClass("EditProfileWidget", {
-  attributes: {
-    widget: ["widgetlist", { only: "TeaserBoxWidget" }],
-  },
-});
-
-export default EditProfileWidget as any;
