eb8baa78b6a9ffb25f1a84aeb7f86189a68c50cb
majedidilbi <majed.idlby@gmail.com>
2022-07-07 16:13:36 +0200

    chore: add CSS to tickets widget

--- src/Widgets/TicketsWidget/TicketsWidgetComponent.tsx
+++ src/Widgets/TicketsWidget/TicketsWidgetComponent.tsx
@@ -43,7 +43,7 @@ Scrivito.provideComponent("TicketsWidget", (({ widget }) => {
   ).first();
 
   return (
-    <Scrivito.WidgetTag className="row equal">
+    <Scrivito.WidgetTag className="row equal sdk-widget sdk-tickets-widget">
       <CreateNewTicket
         className={boxClassName}
         chatPage={chatPage}
