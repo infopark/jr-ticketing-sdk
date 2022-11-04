8ab7c13abaaa987a18393651c4cd2dfa260c3d24
Joergen Dahlke <joergen.dahlke@justrelate.com>
2022-11-04 14:30:47 +0100

    refactor api calls

---
+++ src/api/callApi.ts
@@ -0,0 +1,85 @@
+import axios from "axios";
+
+const METHODS_WITH_BODY = ["POST", "PUT", "PATCH"];
+
+async function callApi(method, url, data = {}) {
+  const options: any = {
+    method: method,
+    url: url,
+    withCredentials: true,
+  };
+
+  if (METHODS_WITH_BODY.includes(method.toUpperCase())) {
+    options.data = data
+  }
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error: any) {
+    return checkIamAuthRedirect(error.response);
+  }
+}
+
+const RETURN_TO_PLACEHOLDER = "$RETURN_TO";
+const API_LOCATION_PLACEHOLDER = "$JR_API_LOCATION";
+const IAM_MISSING_AUTH_CODE = "auth_missing";
+const IAM_MISSING_AUTH_CODE_LEGACY = "auth.missing";
+const IAM_REDIRECT_TIMEOUT_MS = 5000; // within 5 seconds, second of consecutive iam redirects blocked
+const LS_LAST_REDIRECT_TIMESTAMP = "last-iam-redirect-timestamp";
+
+function checkIamAuthRedirect(response) {
+  const responseStatus = response && response.status;
+  const synthResponse = {
+    failedRequest: true,
+    statusCode: responseStatus || 404,
+    tooManyIamRedirects: false,
+    redirectUrl: undefined
+  };
+  if (responseStatus !== 401) {
+    return synthResponse;
+  }
+  const result = response.data;
+  if (
+    result.code !== IAM_MISSING_AUTH_CODE &&
+    result.code !== IAM_MISSING_AUTH_CODE_LEGACY
+  ) {
+    return synthResponse;
+  }
+
+  const apiUrl = process.env.API_BASE_URL;
+  const url = result.details.visit
+    .replace(RETURN_TO_PLACEHOLDER, encodeURIComponent(window.location.href))
+    .replace(API_LOCATION_PLACEHOLDER, apiUrl);
+
+  if (isRepeatedIamRedirect()) {
+    synthResponse.statusCode = 421;
+    synthResponse.tooManyIamRedirects = true;
+    synthResponse.redirectUrl = url;
+    return synthResponse;
+  }
+  setIamRedirectTimestamp();
+
+  window.location.href = `${url}`;
+
+  synthResponse.statusCode = 200;
+  return synthResponse;
+}
+
+function isRepeatedIamRedirect() {
+  const timestamp = parseInt(
+    localStorage.getItem(LS_LAST_REDIRECT_TIMESTAMP)!,
+    10
+  );
+  if (isNaN(timestamp)) {
+    return false;
+  }
+  const timeDiffMs = Date.now() - timestamp;
+  return timeDiffMs < IAM_REDIRECT_TIMEOUT_MS;
+}
+
+function setIamRedirectTimestamp() {
+  localStorage.setItem(LS_LAST_REDIRECT_TIMESTAMP, Date.now().toString());
+}
+
+export { callApi };
--- src/api/portalApiCalls.ts
+++ src/api/portalApiCalls.ts
@@ -1,44 +1,51 @@
-import axios from "axios";
+import { callApi } from "./callApi";
 import { isMockEnabled, mockGet, mockPost } from "./mockEndpoint";
 import { isLocalhost } from "../utils/constants";
 
-const callApiPost = async (endpoint, body) => {
-  if (isMockEnabled()) {
-    return mockPost(endpoint, body);
-  }
-  const instanceId = process.env.API_INSTANCE_ID || "00000000000000000000000000000000";
-  const stage = process.env.API_DEPLOYMENT_STAGE || "develop";
-  const apiUrl = process.env.API_BASE_URL;
-  const config = { withCredentials: true };
-
-  try {
-    const result = await axios
-      .post(`${apiUrl}/${stage}/${instanceId}/${endpoint}`, body, config)
-      .then((response) => response.data);
-    return result;
-  } catch (error: any) {
-    return checkIamAuthRedirect(error.response);
-  }
-};
 
 const callApiGet = async (endpoint) => {
   if (isMockEnabled()) {
     return mockGet(endpoint);
   }
-  const instanceId = process.env.API_INSTANCE_ID || "00000000000000000000000000000000";
-  const stage = process.env.API_DEPLOYMENT_STAGE || "develop";
+  const instanceId = process.env.SCRIVITO_TENANT;
   const apiUrl = process.env.API_BASE_URL;
-  const config = { withCredentials: true };
 
-  try {
-    const result = await axios
-      .get(`${apiUrl}/${stage}/${instanceId}/${endpoint}`, config)
-      .then((response) => response.data);
-    return result;
-  } catch (error: any) {
-    return checkIamAuthRedirect(error.response);
-  }
+  return await callApi("get", `${apiUrl}/ticketing/${instanceId}/${endpoint}`);
 };
+
+const callApiPost = async (endpoint, data) => {
+  if (isMockEnabled()) {
+    return mockPost(endpoint, data);
+  }
+
+  const instanceId = process.env.SCRIVITO_TENANT;
+  const apiUrl = process.env.API_BASE_URL;
+
+  return await callApi("post", `${apiUrl}/ticketing/${instanceId}/${endpoint}`, data);
+};
+
+const callApiPut = async (endpoint, data) => {
+  if (isMockEnabled()) {
+    return mockPost(endpoint, data);
+  }
+
+  const instanceId = process.env.SCRIVITO_TENANT;
+  const apiUrl = process.env.API_BASE_URL;
+
+  return await callApi("put", `${apiUrl}/ticketing/${instanceId}/${endpoint}`, data);
+};
+
+const callApiDelete = async (endpoint, data) => {
+  if (isMockEnabled()) {
+    return mockPost(endpoint, data);
+  }
+
+  const instanceId = process.env.SCRIVITO_TENANT;
+  const apiUrl = process.env.API_BASE_URL;
+
+  return await callApi("delete", `${apiUrl}/ticketing/${instanceId}/${endpoint}`);
+};
+
 
 const callLogout = () => {
   if (isMockEnabled()) {
@@ -57,66 +64,4 @@ const getApiTargetUrl = () => {
   return `${window.location.protocol}//jr-api.${domain}`;
 };
 
-const RETURN_TO_PLACEHOLDER = "$RETURN_TO";
-const API_LOCATION_PLACEHOLDER = "$JR_API_LOCATION";
-const IAM_MISSING_AUTH_CODE = "auth_missing";
-const IAM_MISSING_AUTH_CODE_LEGACY = "auth.missing";
-const IAM_REDIRECT_TIMEOUT_MS = 5000; // within 5 seconds, second of consecutive iam redirects blocked
-const LS_LAST_REDIRECT_TIMESTAMP = "last-iam-redirect-timestamp";
-
-function checkIamAuthRedirect(response) {
-  const responseStatus = response && response.status;
-  const synthResponse = {
-    failedRequest: true,
-    statusCode: responseStatus || 404,
-    tooManyIamRedirects: false,
-    redirectUrl: undefined
-  };
-  if (responseStatus !== 401) {
-    return synthResponse;
-  }
-  const result = response.data;
-  if (
-    result.code !== IAM_MISSING_AUTH_CODE &&
-    result.code !== IAM_MISSING_AUTH_CODE_LEGACY
-  ) {
-    return synthResponse;
-  }
-
-  const apiTargetUrl = getApiTargetUrl();
-  let url = result.details.visit;
-  url = url
-    .replace(RETURN_TO_PLACEHOLDER, encodeURIComponent(window.location.href))
-    .replace(API_LOCATION_PLACEHOLDER, `${apiTargetUrl}`);
-
-  if (isRepeatedIamRedirect()) {
-    synthResponse.statusCode = 421;
-    synthResponse.tooManyIamRedirects = true;
-    synthResponse.redirectUrl = url;
-    return synthResponse;
-  }
-  setIamRedirectTimestamp();
-
-  window.location.href = `${url}`;
-
-  synthResponse.statusCode = 200;
-  return synthResponse;
-}
-
-function isRepeatedIamRedirect() {
-  const timestamp = parseInt(
-    localStorage.getItem(LS_LAST_REDIRECT_TIMESTAMP)!,
-    10
-  );
-  if (isNaN(timestamp)) {
-    return false;
-  }
-  const timeDiffMs = Date.now() - timestamp;
-  return timeDiffMs < IAM_REDIRECT_TIMEOUT_MS;
-}
-
-function setIamRedirectTimestamp() {
-  localStorage.setItem(LS_LAST_REDIRECT_TIMESTAMP, Date.now().toString());
-}
-
-export { callApiPost, callApiGet, callLogout };
+export { callApiGet, callApiPost, callApiPut, callApiDelete, callLogout };
