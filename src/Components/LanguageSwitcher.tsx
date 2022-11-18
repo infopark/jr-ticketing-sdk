e8ce080ed2e08b16147bc80558ac1ed56d26c02e
Dmitry Silchenko <dmytro.silchenko@justrelate.com>
2022-11-18 17:02:59 +0100

    [#183747356] Add language switcher

--- src/Components/LanguageSwitcher.tsx
+++ src/Components/LanguageSwitcher.tsx
@@ -1,62 +1,54 @@
-import React, { useState } from "react";
+import React from "react";
 import * as Scrivito from "scrivito";
 import classNames from "classnames";
-import { useUserData } from "./UserDataContext";
-import { callApiPost } from "../api/portalApiCalls";
-import getUserData from "../api/getUserData";
 import { translate } from "../utils/translate";
-import { getLanguage, getLanguageVersions, getLinkForVersion } from "../utils/page";
-import { mapLocale } from "../utils/dateUtils";
+import { getLanguageVersions, getLinkForVersion } from "../utils/page";
+import i18n from "../config/i18n";
+
+const languageChangedEvent = new Event("language-changed");
 
 const LanguageSwitcher = () => {
-  const { userData, updateUserData } = useUserData();
-
-  const currentLanguage = getLanguage();
+  const currentLanguage = i18n.language;
   const versions = getLanguageVersions();
 
-  if (versions.length <= 1) {
-    return null;
+  const currentPage = Scrivito.currentPage();
+  let languages = {
+    en: currentPage,
+    de: currentPage
+  }
+  versions.forEach((version) => {
+    const lang = version.language();
+    if (lang) {
+      const link = new Scrivito.Link({ url: getLinkForVersion(version), target: null as any });
+      languages[lang] = currentLanguage !== lang ? link : currentPage;
+    }
+  })
+
+  const switchLanguage = (lang: string) => {
+    window.localStorage.language = lang;
+    window.dispatchEvent(languageChangedEvent);
   }
 
-  const persistLanguage = async (lang) => {
-    if (userData.language === lang) {
-      return;
-    }
-    const response = await callApiPost(`update-user/${userData.userid}`, {
-      language: lang,
-      timelocale: mapLocale(lang),
-    });
-    if (response) {
-      if (!response.failedRequest) {
-        const data = await getUserData();
-        updateUserData(data);
-      }
-    }
-  };
   return (
     <>
       <hr />
-      {versions.map((version) => {
-        const lang = version.language();
-        const link = new Scrivito.Link({ url: getLinkForVersion(version), target: null as any });
-        return (
-          <Scrivito.LinkTag
-            to={(currentLanguage !== lang ? link : Scrivito.currentPage()) as any}
-            target={null as any}
-            key={lang}
-            title={lang}
-            onClick={() => persistLanguage(lang)}
-          >
-            <i
-              className={classNames("fa mr-3", {
-                "fa-dot-circle-o": currentLanguage === lang,
-                "fa-circle-o": currentLanguage !== lang,
-              })}
-            />
-            <span>{translate(lang as any)}</span>
-          </Scrivito.LinkTag>
-        );
-      })}
+      {Object.entries(languages).map(([lang, link]) => (
+        <Scrivito.LinkTag
+          to={link}
+          target={null as any}
+          key={lang}
+          title={lang}
+          onClick={() => lang && switchLanguage(lang)}
+        >
+          <i
+            className={classNames("fa mr-3", {
+              "fa-dot-circle-o": currentLanguage === lang,
+              "fa-circle-o": currentLanguage !== lang,
+            })}
+          />
+          <span>{translate(lang as any)}</span>
+        </Scrivito.LinkTag>
+      ))}
     </>
   );
 };
