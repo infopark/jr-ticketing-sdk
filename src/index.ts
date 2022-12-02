aa32836913777f4faffe5ca59f510e29ca7f0a2a
Joergen Dahlke <joergen.dahlke@justrelate.com>
2022-12-02 11:29:13 +0100

    remove UserDataContextProvider

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-import isLoggedIn from "./Components/Auth/utils";
-import UserProfile from "./Components/Layout/UserProfile";
-import ErrorAPIProvider from "./Components/ErrorAPIContext";
-import ErrorNotification from "./Components/ErrorNotification";
-import LanguageRedirect from "./Components/LanguageRedirect";
-import { UserDataProvider, useUserData } from "./Components/UserDataContext";
 import { TenantContextProvider, useTenantContext } from "./Components/TenantContextProvider";
 import "./Objs/ChatPage/ChatPageComponent";
 import "./Objs/ChatPage/ChatPageEditingConfig";
 import "./Objs/ChatPage/ChatPageObjClass";
-import "./Objs/ProfilePage/ProfilePageComponent";
-import "./Objs/ProfilePage/ProfilePageEditingConfig";
-import "./Objs/ProfilePage/ProfilePageObjClass";
 import "./Objs/TicketFormConfiguration/TicketFormConfigurationEditingConfig";
 import "./Objs/TicketFormConfiguration/TicketFormConfigurationObjClass";
-import { translate, getLanguage } from "./utils/translate";
-import { isInVisitedPages, addToVisitedPages } from "./utils/visitedPages";
 import "./Widgets/HistoryWidget/HistoryWidgetClass";
 import "./Widgets/HistoryWidget/HistoryWidgetComponent";
 import "./Widgets/HistoryWidget/HistoryWidgetEditingConfig";
@@ -24,21 +13,8 @@ import "./Widgets/TicketListWidget/TicketListWidgetEditingConfig";
 import "./Widgets/TicketsWidget/TicketsWidgetClass";
 import "./Widgets/TicketsWidget/TicketsWidgetComponent";
 import "./Widgets/TicketsWidget/TicketsWidgetEditingConfig";
-import * as InitialContentBodyFactory from "./Bridge/InitialContentBodyFactory";
 
 export {
-  UserDataProvider,
-  useUserData,
-  UserProfile,
-  LanguageRedirect,
   TenantContextProvider,
   useTenantContext,
-  ErrorAPIProvider,
-  ErrorNotification,
-  isLoggedIn,
-  translate,
-  getLanguage,
-  isInVisitedPages,
-  addToVisitedPages,
-  InitialContentBodyFactory
 };
