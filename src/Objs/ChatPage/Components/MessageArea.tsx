adf1d95e9a2098f28b253563453c9273b242e2b1
Joergen Dahlke <joergen.dahlke@justrelate.com>
2023-01-12 09:10:13 +0100

    [fix] adding multiple attachments at once

--- src/Objs/ChatPage/Components/MessageArea.tsx
+++ src/Objs/ChatPage/Components/MessageArea.tsx
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
 import classNames from "classnames";
+import { isEmpty } from "lodash-es";
 
 import { callApiPost } from "../../../api/portalApiCalls";
 import { MAX_ATTACHMENT_SIZE } from "../../../utils/constants";
 import { useTenantContext } from "../../../Components/TenantContextProvider";
 import newlinesToBreaks from "../../../utils/newlinesToBreaks";
 import i18n from "../../../config/i18n";
+import { FileObject, Keyable } from "../../../utils/types";
 
 const MessageArea = ({ ticketId, refreshCallback, isClosed }) => {
   const { userId } = useTenantContext();
   const [message, setMessage] = useState("");
-  const [file, setFile] = useState("" as any);
-  const [textareaHeight, setTextareaHeight] = useState<number | null>(null);
-  const [attachmentFileName, setAttachmentFileName] = useState<string | null>(
-    null
-  );
-  const [attachmentLoading, setAttachmentLoading] = useState(false);
-  const [attachmentTooBig, setAttachmentTooBig] = useState(false);
+  const [files, setFiles] = useState<FileObject[]>([]);
+  const [textareaHeight, setTextareaHeight] = useState<number>(85);
 
-  const messageOrAttachment = message || file;
+  const filesError = files.some((f: Keyable) => !isEmpty(f.error));
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
@@ -32,94 +29,118 @@ const MessageArea = ({ ticketId, refreshCallback, isClosed }) => {
     }
   };
 
-  const handleSendMessage = async (msg: string) => {
-    if (!isClosed) {
-      const msgData = {
-        text: newlinesToBreaks(msg),
-        user_id: userId,
-        attachments: [] as object[],
-      };
-
-      if (attachmentFileName) {
-        msgData.attachments.push({
-          filename: attachmentFileName
-        });
-      }
-
-      callApiPost(`tickets/${ticketId}/messages`, msgData).then((response) => {
-        setTextareaHeight(null);
-        setFile("");
-        setMessage("");
-        setAttachmentFileName(null);
-        if (!response.failedRequest) {
-          refreshCallback();
-        }
-        return response;
-      });
+  const onSubmit = async () => {
+    if (isClosed) {
+      return;
     }
+
+    const attachments = files.reduce(
+      (result: object[], file) => file.filename
+        ? [...result, { filename: file.filename}]
+        : result, []);
+
+    const msgData = {
+      text: newlinesToBreaks(message),
+      user_id: userId,
+      attachments: attachments
+    };
+
+
+    callApiPost(`tickets/${ticketId}/messages`, msgData).then((response) => {
+      setTextareaHeight(85);
+      setFiles([]);
+      setMessage("");
+      if (!response.failedRequest) {
+        refreshCallback();
+      }
+      return response;
+    });
   };
 
-  const handleFileChange = async (e) => {
-    if (!isClosed) {
-      const uploadFile = e.target.files[0];
-      const size = (uploadFile && uploadFile.size) || 0;
+  function updateFiles(fileObject) {
+    setFiles((files) => files.map((f) => f === fileObject ? fileObject : f));
+  }
 
-      if (!uploadFile) {
-        setAttachmentTooBig(false);
-        return;
-      }
+  const onFileChange = async (e) => {
+    if (isClosed) {
+      return;
+    }
+
+    const fileList: FileList = e.target.files;
+
+    Array.from(fileList).forEach(async (file: Keyable) => {
+      const size = file.size || 0;
+      const fileObject: FileObject = {
+        name: file.name,
+        filename: "",
+        loading: true,
+        error: ""
+      };
+      setFiles(files => [...files, fileObject]);
 
       if (size > MAX_ATTACHMENT_SIZE) {
-        setAttachmentTooBig(true);
+        fileObject.error = "file_too_big";
+        fileObject.loading = false;
+        updateFiles(fileObject);
         return;
       }
 
-      setFile(uploadFile);
-      setAttachmentTooBig(false);
-      setAttachmentLoading(true);
-
-      callApiPost("signed-upload-url", {
-        filename: uploadFile.name,
-      }).then(async (response) => {
-        if (response.failedRequest) {
-          setAttachmentLoading(false);
-          return;
-        }
-        await fetch(response.url, {
-          method: "PUT",
-          body: uploadFile,
-        });
-        setAttachmentFileName(response.filename);
-        setAttachmentLoading(false);
+      const signedResult = await callApiPost("signed-upload-url", {
+        filename: file.name,
       });
-      e.target.value = "";
-    }
+
+      if (signedResult.failedRequest) {
+        fileObject.error = "file_upload_failed";
+        fileObject.loading = false;
+        updateFiles(fileObject);
+        return;
+      }
+
+      const uploadResult = await fetch(signedResult.url, {
+        method: "PUT",
+        body: file as BodyInit,
+      });
+
+      if (uploadResult.status >= 200 && uploadResult.status < 300) {
+        fileObject.filename = signedResult.filename;
+      } else {
+        fileObject.error = "file_upload_failed";
+      }
+
+      fileObject.loading = false;
+      updateFiles(fileObject);
+    });
   };
+
+  function removeUpload(file: object) {
+    setFiles(files.filter(f => f !== file));
+  }
 
   return (
     <div className="sdk sdk-chat-page">
       <section className="message_box">
-        {file && (
-          <div className="attachment_file">
-            <div className={classNames("dots", { loading: attachmentLoading })}>
+        {files.map((file: Keyable) => (
+          <div className="attachment_file mb-0" key={file.name}>
+            <div className={classNames("dots", { loading: file.loading })}>
               {file.name}
-              {attachmentLoading && " (processing)"}
-              {attachmentLoading && <div className="bg" />}
+              {" "}
+              {file.loading && i18n.t("MessageArea.processing_file")}
+              {!isEmpty(file.error) && i18n.t(`MessageArea.${file.error}`)}
             </div>
-            <div className="delete_file pointer" onClick={() => setFile("")}>
+            <div className="delete_file pointer" onClick={() => removeUpload(file)}>
               x
             </div>
           </div>
-        )}
+        ))}
         <div className="flex_grid">
           <div className="textfield">
             <textarea
               className="form-control dialog_text_area resize-ta"
-              placeholder={i18n.t("your message ...")}
+              placeholder={i18n.t("MessageArea.message_placeholder")}
               value={message}
               onChange={handleChange}
               onKeyUp={handleKeys}
-              style={{ height: textareaHeight || "45px" }}
+              style={{ height: textareaHeight, minHeight: "85px" }}
               disabled={isClosed}
             />
           </div>
@@ -130,23 +151,24 @@ const MessageArea = ({ ticketId, refreshCallback, isClosed }) => {
                 isClosed ? " disabled" : ""
               }`}
             >
-              {i18n.t("attach file")}
+              {i18n.t("MessageArea.attach_file")}
               <input
                 type="file"
                 id="fileUpload"
                 name="fileUpload"
-                onChange={(e) => handleFileChange(e)}
-                hidden
+                onChange={onFileChange}
                 disabled={isClosed}
+                hidden
+                multiple
               />
             </label>
             <button
               className="btn btn-primary btn_outline float_right"
-              onClick={() => handleSendMessage(message)}
-              disabled={isClosed || attachmentLoading || attachmentTooBig || !messageOrAttachment}
+              onClick={onSubmit}
+              disabled={isClosed || !message || filesError}
               type="button"
             >
-              {i18n.t("post message")}
+              {i18n.t("MessageArea.submit")}
             </button>
           </div>
         </div>
