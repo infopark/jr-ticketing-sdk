e06aeee66ad037252c6f36fdf0df0626e52db8c9
Dmitry Silchenko <81808117+dmitry-silchenko@users.noreply.github.com>
2023-04-25 09:42:53 +0200

    [#184842035] Redesign Add Ticket dialog (#58)
    [#184842035] Redesign Add Ticket dialog

--- src/Widgets/TicketsWidget/CreateNewTicketOverlay.tsx
+++ src/Widgets/TicketsWidget/CreateNewTicketOverlay.tsx
@@ -10,10 +10,12 @@ import Form from "@rjsf/core";
 import i18n from "../../config/i18n";
 import Loader from "../../Components/Loader";
 import TicketingApi from "../../api/TicketingApi";
-import FooterButtons from "./FooterButtons";
 import { useTicketingContext } from "../../Components/TicketingContextProvider";
 import { MAX_ATTACHMENT_SIZE } from "../../utils/constants";
 import { FileObject, Keyable } from "../../utils/types";
+
+import AttachIcon from "../../assets/images/icons/attach_icon.svg";
+import SendIcon from "../../assets/images/icons/send_icon.svg";
 
 const CustomAttachment = function({ id, value, onChange }) {
   const { addError } = useTicketingContext();
@@ -144,14 +146,22 @@ function CreateNewTicketOverlay({
   const [loading, setLoading] = React.useState(false);
   const [showError, setShowError] = React.useState(false);
 
+  const [message, setMessage] = React.useState("");
+  const [files, setFiles] = React.useState<FileObject[]>([]);
+  const [rows, setRows] = React.useState<number>(1);
+
+  const filesError = files.some((f: Keyable) => !isEmpty(f.error));
+
+  const { currentUser, addError } = useTicketingContext();
+
   const renderBackdrop = (props) => (
     <div className="mute_bg_2" {...props} />
   );
 
-  const { currentUser, addError } = useTicketingContext();
-
   function resetForm() {
     setFormData({});
+    setMessage("");
+    setFiles([]);
   }
 
   const onSubmitForm = async () => {
@@ -160,9 +170,9 @@ function CreateNewTicketOverlay({
     try {
       const ticketAttributes = {};
       const messageAttributes = {
-        text: formData["message.text"],
-        attachments: formData["message.attachments"]?.map(attachment => ({
-          filename: attachment,
+        text: message,
+        attachments: files.map(file => ({
+          filename: file.filename,
         }))
       };
 
@@ -187,8 +197,12 @@ function CreateNewTicketOverlay({
         return;
       }
 
-      Scrivito.navigateTo(ticketPage, {
-        id: String(newTicket.number),
+      resetForm();
+
+      setTimeout(() => {
+        Scrivito.navigateTo(ticketPage, {
+          id: String(newTicket.number),
+        });
       });
     } catch (error) {
       addError("Error submitting ticket", "CreateNewTicketOverlay", error);
@@ -196,6 +210,85 @@ function CreateNewTicketOverlay({
       setShowError(true);
     }
   };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+
+    const { paddingTop, paddingBottom, lineHeight } = getComputedStyle(e.target);
+
+    const previousRows = e.target.rows;
+    e.target.rows = 1;
+
+    const currentRows = Math.floor((e.target.scrollHeight - parseFloat(paddingTop) - parseFloat(paddingBottom)) / parseFloat(lineHeight));
+
+    if (currentRows === previousRows) {
+      e.target.rows = currentRows;
+    }
+
+    setRows(currentRows);
+  };
+
+  function updateFiles(fileObject) {
+    setFiles((files) => files.map((f) => f === fileObject ? fileObject : f));
+  }
+
+  const onFileChange = async (e) => {
+    const fileList: FileList = e.target.files;
+
+    Array.from(fileList).forEach(async (file: Keyable) => {
+      const size = file.size || 0;
+      const fileObject: FileObject = {
+        name: file.name,
+        size: file.size,
+        filename: "",
+        loading: true,
+        error: ""
+      };
+      setFiles(files => [...files, fileObject]);
+
+      if (size > MAX_ATTACHMENT_SIZE) {
+        fileObject.error = "file_too_big";
+        fileObject.loading = false;
+        updateFiles(fileObject);
+        return;
+      }
+
+      try {
+        const signedResult = await TicketingApi.post("signed-upload-url", {
+          data: {
+            filename: file.name,
+          }
+        });
+
+        if (!signedResult) {
+          fileObject.error = "file_upload_failed";
+          fileObject.loading = false;
+          updateFiles(fileObject);
+          return;
+        }
+
+        const uploadResult = await fetch(signedResult.url, {
+          method: "PUT",
+          body: file as BodyInit,
+        });
+
+        if (uploadResult.status >= 200 && uploadResult.status < 300) {
+          fileObject.filename = signedResult.filename;
+        } else {
+          fileObject.error = "file_upload_failed";
+        }
+
+        fileObject.loading = false;
+        updateFiles(fileObject);
+      } catch (error) {
+        addError("Error uploading file", "MessageArea", error);
+      }
+    });
+  };
+
+  function removeUpload(file: object) {
+    setFiles(files.filter(f => f !== file));
+  }
 
   const [schema, setSchema] = React.useState({});
   const [uiSchema, setUiSchema] = React.useState({});
@@ -275,6 +368,11 @@ function CreateNewTicketOverlay({
           <div className="ticket-modal-form">
             <div className="overlay_content scroll_content">
               <h2>{i18n.t("CreateNewTicket.create_new_ticket")}</h2>
+              <button type="button"
+                className="btn-close"
+                aria-label={i18n.t("close")}
+                onClick={(e) => leavingConfirmation() && (resetForm(), close(e))}
+              />
               <div className="inline_form">
                 <Form
                   formData={formData}
@@ -296,11 +394,81 @@ function CreateNewTicketOverlay({
                 </div>
               )}
             </div>
-            <FooterButtons
-              onSubmit={onSubmitForm}
-              onCancel={(e) => leavingConfirmation() && (resetForm(), close(e))}
-              disabled={false}
-            />
+
+            <footer id="overlay_footer" className="message_box">
+              <div className="mb-4">
+                {files.map((file: Keyable, index: number) => (
+                  <div className="file-loading-card" key={`${index}-${file.filename}`}>
+                    <div className="card">
+                      <div className="card-body">
+                        <div className="inline-nowrap-area">
+                          <i className="fa-regular fa-file pe-2"></i>
+                          <h5 className="card-title dots">
+                            {decodeURIComponent(file.name)}
+                          </h5>
+
+                          <p className="card-text">
+                            {Math.round(file.size / 1024)}KB
+                          </p>
+
+                          {file.loading && (
+                            <div className="progress">
+                              <div className="progress-bar" role="progressbar" style={{ width: "25%" }} aria-valuenow={25} aria-valuemin={0} aria-valuemax={100} />
+                            </div>
+                          )}
+
+                          {!isEmpty(file.error) && i18n.t(`CreateNewTicket.${file.error}`)}
+                        </div>
+                        <button className="btn" type="button" aria-label="Delete" onClick={() => removeUpload(file)}>
+                          <i className="fa-regular fa-circle-xmark" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="flex_grid">
+                <div className="textfield_btn">
+                  <button className="btn btn-secondary" type="button">
+                    <label
+                      htmlFor="fileUpload"
+                      className="btn btn-secondary m-0 p-0 with-btn-lnf"
+                    >
+                      <img className="img-fluid" src={AttachIcon} />
+                      <input
+                        type="file"
+                        id="fileUpload"
+                        name="fileUpload"
+                        onChange={onFileChange}
+                        hidden
+                        multiple
+                      />
+                    </label>
+                  </button>
+                </div>
+                <div className="textfield">
+                  <textarea className="form-control dialog_text_area resize-ta"
+                    placeholder={i18n.t("CreateNewTicket.message_placeholder")}
+                    value={message}
+                    onChange={handleChange}
+                    rows={rows}
+                  />
+                </div>
+                <div className="textfield_btn">
+                  <button className="btn btn-primary float-lg-right"
+                    type="button"
+                    onClick={onSubmitForm}
+                    disabled={!formData["title"] || !message || filesError}
+                  >
+                    <img className="img-fluid" src={SendIcon} />
+                    <span className="d-none d-lg-inline-block">
+                      {i18n.t("Create")}
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </footer>
           </div>
         </section>
       </div>
